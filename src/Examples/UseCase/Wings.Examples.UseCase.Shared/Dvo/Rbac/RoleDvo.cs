519e78356ed4a45a2ca1ba6113a4b5b90b47500f
zhuchediyici <2418819696@qq.com>
2021-07-08 12:34:47 +0800

    修改了角色权限

--- src/Examples/UseCase/Wings.Examples.UseCase.Shared/Dvo/Rbac/RoleDvo.cs
+++ src/Examples/UseCase/Wings.Examples.UseCase.Shared/Dvo/Rbac/RoleDvo.cs
@@ -9,79 +9,53 @@ using Wings.Framework.Shared.Dtos.Admin;
 
 namespace Wings.Examples.UseCase.Shared.Dvo
 {
+    /// <summary>
+    /// 声明一个类 角色页面 继承 页面设计
+    /// </summary>
     public class RolePage : PageDesign
     {
-
-
         public override PageData Design()
         {
             return SetPageTitle("角色管理")
-                    .SetPageLink("/rbac/role2")
-                  .SetMainView<RoleListView>()
-                  .SetCreateViewType<RoleListView>()
-                  .SetCreateViewTabs(ManyToMany<MenuListDvo>(nameof(RoleListView.Menus), "角色菜单"), ManyToMany<PermissionListDvo>(nameof(RoleListView.Permissions), "角色权限"))
-                  .SetUpdateViewTabs(ManyToMany<MenuListDvo>(nameof(RoleListView.Menus), "角色菜单"), ManyToMany<PermissionListDvo>(nameof(RoleListView.Permissions), "角色权限"))
-                  .SetUpdateViewType<RoleListView>()
-                  .SetDetailViewType<RoleListView>()
-                  .SetDetailViewTabs(ManyToMany<MenuListDvo>(nameof(RoleListView.Menus), "角色菜单"), ManyToMany<PermissionListDvo>(nameof(RoleListView.Permissions), "角色权限"))
-                  .Commit();
+            .SetPageLink("/rbac/role2")
+            .SetCreateViewType<RoleListView>()
+            .SetMainView<RoleListView>()
+            .SetUpdateViewType<RoleListView>()
+            .SetDetailViewType<RoleListView>()
+            .SetCreateViewTabs(ManyToMany<MenuListDvo>(nameof(RoleListView.Menus), "角色菜单"), ManyToMany<PermissionListDvo>("Permissions", "角色权限"))
+            .SetUpdateViewTabs(ManyToMany<MenuListDvo>(nameof(RoleListView.Menus), "角色菜单"), ManyToMany<PermissionListDvo>("Permissions", "角色权限"))
+            .SetDetailViewTabs(ManyToMany<MenuListDvo>(nameof(RoleListView.Menus), "角色菜单"), ManyToMany<PermissionListDvo>("Permissions", "角色权限"))
+            .Commit();
         }
     }
-
-
-
+    [DataSource("/api/roleView")]
     [SearchBar(typeof(RoleListSearchBar))]
-    [DataSource("/api/RoleView")]
-    [TablePage("角色管理")]
-    [Display(Name = "角色管理")]
+
     public class RoleListView
     {
         [Key]
         [FormField(Edit = false)]
         public int Id { get; set; }
-
-
+        [Display(Name = "角色代码")]
+        [Required]
+        public string Code { get; set; }
         [Display(Name = "角色名")]
         [Required]
         public string Name { get; set; }
-
-        [Display(Name = "角色代码")]
+        [Display(Name = "规范化名称")]
         [Required]
-
-        public string Code { get; set; }
-
+        public string NormalizedName { get; set; }
         [Ignore]
-        [Display(Name = "创建时间")]
-        public DateTime CreateAt { get; set; } = DateTime.Now;
-
-
-        [Ignore]
-        public List<MenuListDvo> Menus { get; set; }
+        public List<MenuListDvo> Menus { get; set; } = new List<MenuListDvo>();
         [Ignore]
         public List<PermissionListDvo> Permissions { get; set; }
-
     }
 
     public class RoleListSearchBar
     {
-        [Display(Name = "角色名")]
         [Where(WhereCondition.Contains)]
         public string Name { get; set; }
-        [Display(Name = "角色代码")]
-        [Where(WhereCondition.Contains)]
-        public string Code { get; set; }
-        [Display(Name = "创建时间")]
-        public DateRange CreateDateBetween
-        {
-            get { return new DateRange(CreateAtStart, CreateAtEnd); }
-            set { CreateAtStart = value.Start; CreateAtEnd = value.End; }
-        }
-        [Ignore]
-        [Where(WhereCondition.GreatThen, nameof(RoleListDvo.CreateAt))]
-        public DateTime? CreateAtStart { get; set; }
-        [Ignore]
-        [Where(WhereCondition.LessThen, nameof(RoleListDvo.CreateAt))]
-        public DateTime? CreateAtEnd { get; set; }
+
     }
 
 
